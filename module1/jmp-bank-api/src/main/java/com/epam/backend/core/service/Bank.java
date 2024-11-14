1e5dbef60706641afbe0d30e82033987b9783f77
Magdalena <Magdalena_Zaba@epam.com>
2024-11-14 17:11:03 +0100

    init commit

--- module1/jmp-bank-api/src/main/java/com/epam/backend/core/service/Bank.java
+++
@@ -1,9 +0,0 @@
-package com.epam.backend.core.service;
-
-import com.epam.backend.core.dto.BankCard;
-import com.epam.backend.core.dto.BankCardType;
-import com.epam.backend.core.dto.User;
-
-public interface Bank {
-    BankCard createBankCard(User user, BankCardType cardType);
-}
--- module1/jmp-dto/src/main/java/com/epam/backend/core/dto/BankCardType.java
+++
@@ -1,6 +0,0 @@
-package com.epam.backend.core.dto;
-
-public enum BankCardType {
-    CREDIT,
-    DEBIT
-}
